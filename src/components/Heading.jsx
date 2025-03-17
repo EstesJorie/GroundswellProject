53cf491743cb77d5531b0ebb73af9ce3df97d3a6
tomparkinson <tomparkinson12@gmail.com>
2025-03-17 20:07:19 +0000

    commit

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -4,7 +4,7 @@ export default function Heading() {
   return (
     <div className='flex flex-row bg-blue-300'>
         <img className='h-15' src="../groundswell.png" alt="Groundswell logo" />
-        <span className='font-bold text-4xl p-2'>Groundswell Innovation</span>
+        <span className='font-bold text-4xl p-2 font-serif'>Groundswell Innovation</span>
     </div>
   )
 }
