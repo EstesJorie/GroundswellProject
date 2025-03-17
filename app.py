ddc259c2d6ac028c39a23558a0aa14f29f488af1
EstesJorie <joe.tresise@aol.com>
2025-03-17 19:28:35 +0000

    170325 implemtnation of test groq implementation

--- app.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv('.env')
-APIKEY = os.getenv('GROQ_API_KEY')
-
-''' DO NOT LET THIS KEY BE EXPOSED THIS IS JUST TO TEST!!! '''
-print(f"API Key loaded: {'Key exists' if APIKEY else 'Key not found'}")
-print(f"First 10 characters of key: {APIKEY[:10] if APIKEY else 'N/A'}")
