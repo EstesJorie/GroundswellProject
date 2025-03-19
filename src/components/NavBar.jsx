bbb6ea9a3ab665913a9b97eefde6c4527eee1ba3
JosephB2 <josephbousfield99@gmail.com>
2025-03-19 15:45:19 +0000

    Add gradient effect to NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router'
 
 export default function NavBar() {
   return (
-    <div className="flex-none flex items-center gap-4 p-4 shadow bg-[#345061] text-white w-full">
+    <div className="flex-none flex items-center gap-4 p-4 bg-gradient-to-r from-[#345061] to-[#18252D] text-white w-full">
         <Link to="/">Home</Link>
     </div>
   )
