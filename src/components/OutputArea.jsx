943359c758f842d1694c22e552223b4493d32d87
tomparkinson <tomparkinson12@gmail.com>
2025-03-19 22:53:25 +0000

    got message history working

--- src/components/OutputArea.jsx
+++ src/components/OutputArea.jsx
@@ -1,20 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ChatMessage from './ChatMessage'
 import { useEffect } from 'react'
 
 
 /* not sure if key is meant to be done in this way */
 
-export default function OutputArea({text}) {
-  const messageHistory = [
-    {key:0, sender:"SYSTEM", content:"You are now talking to our **AI Chatbot**"},
-    {key:1, sender:"BOT", content:"Hello World"}
-  ] /* Placeholder */
+export default function OutputArea({text, output}) {
+  const [messageHistory, setMessageHistory] = useState([
+    {key:0, sender:"SYSTEM", content:"You are now talking to our **AI Chatbot**"}
+  ]) // {key:1, sender:"BOT", content:"Hello World"}
+  console.log(typeof messageHistory)
+
 
   useEffect(() => {
-    messageHistory[messageHistory.length-1] 
-    console.log(messageHistory)
+    if (text) {
+      setMessageHistory(prevMessages => [
+        ...prevMessages, 
+        {key: prevMessages.length, sender: "YOU", content: text}
+      ]);
+    }
   }, [text])
+  
+  useEffect(() => {
+    if (output) {
+      setMessageHistory(prevMessages => [
+        ...prevMessages, 
+        {key: prevMessages.length, sender: "BOT", content: output}
+      ]);
+    }
+  }, [output])
+
   return (
     <div id='out' className='grow bg-white flex flex-row'>
       <div className='grid items-center p-5 bg-[#BFC0C0]'>
