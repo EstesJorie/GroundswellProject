5d155ee3783dab4aa0b9b7e4e13d813153c6b74c
tomparkinson <tomparkinson12@gmail.com>
2025-03-19 19:04:37 +0000

    commit

--- src/components/OutputArea.jsx
+++ src/components/OutputArea.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ChatMessage from './ChatMessage'
 
-export default function OutputArea() {
+export default function OutputArea({text, output}) {
   return (
     <div id='out' className='grow bg-white overflow-y-scroll p-4 flex flex-col'>
       <ChatMessage sender={'SYSTEM'} content='You are now talking to our **AI Chatbot**' />
-      <ChatMessage sender={'YOU'} content='Hi Chatbot' />
-      <ChatMessage sender={'BOT'} content='*Hello World*' />
+      <ChatMessage sender={'YOU'} content={text} />
+      <ChatMessage sender={'BOT'} content={output} />
     </div>
   )
 }
