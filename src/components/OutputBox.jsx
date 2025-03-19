07c6020310d578c992bdaec6d92de8f0d413bd1d
tomparkinson <tomparkinson12@gmail.com>
2025-03-19 17:31:40 +0000

    removed outputbox

--- src/components/OutputBox.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function OutputBox({output}) {
-  return (
-    <div className='grid p-4'>
-    <span className="w-full h-50 p-3 mono rounded-lg shadow-sm shadow-gray-300 text-white bg-[#9cceef] mb-2">{output}</span>
-    </div>
-  )
-}
