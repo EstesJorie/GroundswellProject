47ceaeefea3ca836efdd175c5b03ae506b57c3a8
tomparkinson <tomparkinson12@gmail.com>
2025-03-19 17:29:48 +0000

    joseph's

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -59,26 +59,11 @@ export default function Input({ setOutput, output }) {
         //   };
 
 return (
-<<<<<<< HEAD
-        <>
-        <div className='grid'>
-        <form action="submit" id="text-form" className='p-4'>
-            <textarea rows="10" name="text" placeholder='Start typing...' onChange={handleTextChange}
-             className="w-full p-3 mono rounded-lg text-white bg-[#284b63] placeholder-[#528DB7]shadow-lg shadow-gray-500  mb-2" ></textarea>
-             
-             
-             
-        <div className="flow-root">
-            <div className='float-left'>
-                <input id="file" type="file" onChange={handleFileChange} className='border border-black rounded bg-white' />
-            </div>
-=======
   <div className='grid w-full flex-none'>
     <form action="submit" id="text-form" className='p-4'>
       <textarea rows="10" name="text" placeholder='Start typing...' onChange={handleTextChange}
        className="w-full p-3 mono rounded-xs text-black bg-[#E4ECF1] placeholder-[#6A7881] h-25" >
       </textarea>
->>>>>>> f062d6ddb9657fd4d5e1812e0a03297f36abd94d
         
       <div className="flow-root">
           <div className='float-left'>
