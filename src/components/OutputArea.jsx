5e36792a98e6fb6d35af7f640f1fb45313e3d0de
JosephB2 <josephbousfield99@gmail.com>
2025-03-19 18:57:40 +0000

    Add placeholder message history in OutputArea.jsx

--- src/components/OutputArea.jsx
+++ src/components/OutputArea.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import ChatMessage from './ChatMessage'
 
+const messageHistory = [
+  {sender:"SYSTEM", content:"You are now talking to our **AI Chatbot**"},
+  {sender:"BOT", content:"Hello World"}
+] /* Placeholder */
+
 export default function OutputArea() {
   return (
     <div id='out' className='grow bg-white overflow-y-scroll flex flex-row'>
@@ -8,9 +13,7 @@ export default function OutputArea() {
         <p>{':)'}</p>
       </div>
       <div className='flex flex-col grow p-4'>
-        <ChatMessage sender={'SYSTEM'} content='You are now talking to our **AI Chatbot**' />
-        <ChatMessage sender={'YOU'} content='Hi Chatbot' />
-        <ChatMessage sender={'BOT'} content='*Hello World*' />
+        {messageHistory.map((msg) => <ChatMessage sender={msg.sender} content={msg.content} />)}
       </div>
     </div>
   )
